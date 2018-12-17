b0be6cabd45e3ee5c44a9a4fd71e15da704feaa0
DamoM73 <damomurtagh@gmail.com>
2018-12-17 11:29:10 +1000

    Finished student opp activity

---
+++ Unit_1_Part_3/student_details/student non-opp.py
@@ -0,0 +1,10 @@
+studentAge = 14
+studentName = "Mary"
+studentSubject = ["Digital Technologies", "Maths", "English"]
+studentBirthCity = "Cairns"
+studentBirthstate = "Queensland"
+
+print("All about", studentName)
+print(studentName, "is", studentAge, "years old.")
+print(studentName, "studies", studentSubject[0] + ",", studentSubject[1] + ", and" ,studentSubject[2])
+print(studentName, "was born in", studentBirthCity + ",", studentBirthstate)
---
+++ Unit_1_Part_3/student_details/student opp.py
@@ -0,0 +1,19 @@
+class student:
+    def __init__(self, name, age, subject1, subject2, subject3, birthCity, birthState):
+        self.name = name
+        self.age = age
+        self.subjects = [subject1, subject2, subject3]
+        self.birthPlace = [birthCity, birthState]
+
+    def details(self):
+        print("All about", self.name)
+        print(self.name, "is", self.age, "years old")
+        print(self.name, "studies", self.subjects[0] + ",", self.subjects[1] + ", and", self.subjects[2])
+        print(self.name, "was born in", self.birthPlace[0] + ",", self.birthPlace[1])
+
+mary = student("Mary", 14, "Digital Technology", "Maths", "English", "Cairns", "Qld")
+michael = student("Michael", 16, "Digital Solutions", "Specialist Maths", "English", "Brisbane", "Qld")
+
+mary.details()
+print("")
+michael.details()
